94c7744d7abf09ebb190d84e704485b24c07013d
ycjqf <76817515+ycjqf@users.noreply.github.com>
2021-11-10 17:52:42 +0800

    feat: 登出

--- web/src/pages/static/index.vue
+++ web/src/pages/static/index.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 const accessToken = localStorage.getItem("access_token");
 const refreshToken = localStorage.getItem("refresh_token");
+import { useRouter } from "vue-router";
+const router = useRouter();
 const user = accessToken
   ? JSON.parse(
       decodeURIComponent(
@@ -8,6 +10,15 @@ const user = accessToken
       )
     )
   : undefined;
+
+async function logoff() {
+  localStorage.clear();
+  console.log("登出");
+  window.location.href = "/";
+}
 </script>
 
-<template>{{ accessToken && refreshToken ? `已登陆（${user.name}）` : "未登陆" }}</template>
+<template>
+  {{ accessToken && refreshToken ? `已登陆（${user.name}）` : "未登陆" }}
+  <div class="logoff" @click="logoff">登出</div>
+</template>
