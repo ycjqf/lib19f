347b83722a311b8332b8b173c2a3d5393a389c6b
xiaoyang <76817515+ycjqf@users.noreply.github.com>
2022-02-26 15:59:59 +0800

    refactor: 使用类的构造器做表单初始化检查

--- server/src/api/getArticles.ts
+++ server/src/api/getArticles.ts
@@ -1,79 +1,98 @@
 import { Router } from "express";
-import Article from "@/models/Article";
-import {
-  ApiGetArticlesRequest,
-  ApiGetArticlesResponse,
-  ArticlePreview,
-  GetArticlesReq,
-  GetArticlesRes,
-} from "@typings/api";
+import { ApiGetArticlesResponse, ArticlePreview } from "@typings/api";
 import { sendJSONStatus } from "@/util";
 import { DEFAULT_ARTICLE_PAGE_SIZE, POSITIVE_INTEGER_REGEX } from "@typings/constants";
+import Article from "@/models/Article";
 import User from "@/models/User";
 
-const router = Router();
-
-router.post("/", async (req, res) => {
-  const queried: ApiGetArticlesRequest = {
-    page: req.body.page,
-    pageSize: req.body.pageSize,
-  };
-  const havePageButInvalid = queried.page && !POSITIVE_INTEGER_REGEX.test(queried.page);
-  const havePageSizeButInvalid =
-    queried.pageSize && !POSITIVE_INTEGER_REGEX.test(queried.pageSize);
-  if (havePageButInvalid || havePageSizeButInvalid) {
-    return sendJSONStatus<ApiGetArticlesResponse>(res, {
+export default Router().post("/", async (request, response) => {
+  const payload = new GetArticlesPayload(request.body.page, request.body.pageSize);
+  if (!payload.checked) {
+    return sendJSONStatus<ApiGetArticlesResponse>(response, {
       code: "WRONG_QUERY",
-      message: "分页若存在需为正整数",
+      message: payload.message,
       articles: [],
       total: 0,
-      current: 1,
-      pageSize: DEFAULT_ARTICLE_PAGE_SIZE,
+      current: payload.body.page,
+      pageSize: payload.body.pageSize,
     });
   }
-  const newPage =
-    queried.page === null || typeof queried.page === "undefined" ? 1 : parseInt(queried.page);
-  const newPageSize =
-    queried.pageSize === null || typeof queried.pageSize === "undefined"
-      ? DEFAULT_ARTICLE_PAGE_SIZE
-      : parseInt(queried.pageSize);
-
-  const total = await Article.countDocuments({});
-  const articles = await Article.find()
-    .select("id title description poster userId createdTime updatedTime -_id")
-    .limit(newPageSize)
-    .skip((newPage - 1) * newPageSize)
-    .sort("-updatedTime");
-
-  const promises = articles.map(async (art): Promise<ArticlePreview> => {
-    const { id, title, description, poster, userId, createdTime, updatedTime } = art;
-    const user = await User.findOne({ id: userId });
-    const { name, avatar } = user;
-    return {
-      id,
-      title,
-      description,
-      poster,
-      createdTime,
-      updatedTime,
-      profile: {
-        id: userId,
-        name,
-        avatar,
-      },
-    };
-  });
-
-  const populatedArticles: ArticlePreview[] = await Promise.all(promises);
-
-  return sendJSONStatus<ApiGetArticlesResponse>(res, {
-    code: "OK",
-    message: "success",
-    articles: populatedArticles,
-    total,
-    current: newPage,
-    pageSize: newPageSize,
-  });
+  try {
+    const total = await Article.countDocuments({});
+    const articles = await Article.find()
+      .select("id title description poster userId createdTime updatedTime -_id")
+      .limit(payload.body.pageSize)
+      .skip((payload.body.page - 1) * payload.body.pageSize)
+      .sort("-updatedTime");
+    const promises = articles.map(async (art): Promise<ArticlePreview> => {
+      const { id, title, description, poster, userId, createdTime, updatedTime } = art;
+      const user = await User.findOne({ id: userId });
+      const { name, avatar } = user;
+      return {
+        id,
+        title,
+        description,
+        poster,
+        createdTime,
+        updatedTime,
+        profile: {
+          id: userId,
+          name,
+          avatar,
+        },
+      };
+    });
+    return sendJSONStatus<ApiGetArticlesResponse>(response, {
+      code: "OK",
+      message: "ok",
+      articles: await Promise.all(promises),
+      total: total,
+      current: payload.body.page,
+      pageSize: payload.body.pageSize,
+    });
+  } catch (e) {
+    return sendJSONStatus<ApiGetArticlesResponse>(response, {
+      code: "INTERNAL_ERROR",
+      message: `error,${e.message}`,
+      articles: [],
+      total: 0,
+      current: payload.body.page,
+      pageSize: payload.body.pageSize,
+    });
+  }
 });
+class GetArticlesPayload {
+  checked: boolean = false;
+  message: string;
+  body: {
+    page: number;
+    pageSize: number;
+  } = {
+    page: 1,
+    pageSize: DEFAULT_ARTICLE_PAGE_SIZE,
+  };
 
-export default router;
+  constructor(page: any, pageSize: any) {
+    if (typeof page === "string" || typeof page === "number") {
+      const testPage = `${page}`;
+      if (POSITIVE_INTEGER_REGEX.test(testPage)) {
+        this.body.page = parseInt(testPage);
+      } else {
+        this.message = "page should be a positive integer";
+        return;
+      }
+    }
+
+    if (typeof pageSize === "string" || typeof pageSize === "number") {
+      const testPageSize = `${pageSize}`;
+      if (POSITIVE_INTEGER_REGEX.test(testPageSize)) {
+        this.body.pageSize = parseInt(testPageSize);
+      } else {
+        this.message = "page size should be a positive integer";
+        return;
+      }
+    }
+    this.message = "ok";
+    this.checked = true;
+  }
+}
